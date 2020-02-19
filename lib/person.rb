08b46295f6d98120f2e3f1ac0d2d0458d8b331ab
ellisandrews <ellisandrews1@gmail.com>
2020-02-19 15:12:10 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    
+    def name
+        @name
+    end
+
+    def name=(dog_name)
+        @name = dog_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(person_job)
+        @job = person_job
+    end
+
+end
